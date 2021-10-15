91daa08d9e8eec164ce9160830927fbfecf9e68c
wanghaisheng <edwin_uestc@163.com>
2021-10-15 19:20:04 +0800

    Update getrawvideos.py

--- getrawvideos.py
+++ getrawvideos.py
@@ -3,7 +3,7 @@ import json
 
 import yt_dlp
 import os
-
+import subprocess
 
 results =[]
 
@@ -37,28 +37,35 @@ with open('cookies.json') as f:
     session.cookies = cookie_jar
     idlist=[]    
     for i in range(1,20):
-        payload = {'search_type': 'video', 'keyword': '骗局','order':'pubdate','duration':'2','page':i}
+        payload = {'search_type': 'video', 'keyword': '阿瓦达索命咒','order':'pubdate','duration':'0','page':i}
         r = session.get('http://api.bilibili.com/x/web-interface/search/type', params=payload)
         # print(r.json().keys())
         results.extend(r.json()["data"]["result"])
+        # result={"title":'阿瓦达索命咒 compilation'+str(i),
+        # "tag":item['tag'],        
+        # "description":"automatically bot  generate from internet.any copyright issue pls contact us\r\n"}        
+
+        if not os.path.exists('harry'+os.sep+str(i)):
+            os.makedirs('harry'+os.sep+str(i))
+        os.chdir('harry'+os.sep+str(i))
         for i,item in enumerate(r.json()["data"]["result"]):
 
 
-            result={"title":item['title'],
-            "tag":item['tag'],
-            "description":item['description']+"original video from\r\n"+item['arcurl']}
-            if not os.path.exists(str(item['id'])):
-                os.makedirs(str(item['id']))
-            os.chdir(str(item['id']))
-            ydl_opts = {}
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(item['arcurl'])
-                        # ydl.download([item['arcurl']])
 
-                with open(str(item['id'])+'.json', 'w') as outfile:
-                    json.dump(result, outfile)    
-            os.chdir("..")
-            idlist.append(item['arcurl'])
-    print('found videos numbers',len(idlist))
-    with open('videolist.txt', 'w') as outfile:
-        json.dump(idlist, outfile)
+            ydl_opts = {'retries': 10}
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                print('donwloading----',item['arcurl'])
+                # ydl.download(item['arcurl'])
+
+                ydl.download([item['arcurl']])
+        with open('test.sh','w') as f:
+            f.write('for f in *.flv ; do echo file \'$f\' >> list.txt; done && ffmpeg -f concat -safe 0 -i list.txt -s 1280x720 -crf 24 stitched-video.mp4 && rm list.txt')
+            f.write('\r')
+            f.write('for f in *.mp4 ; do echo file \'$f\' >> list.txt; done && ffmpeg -f concat -safe 0 -i list.txt -s 1280x720 -crf 24 stitched-video.mp4 && rm list.txt')
+
+        subprocess.call(['sh', './test.sh'])                        
+ 
+        os.chdir('../../')
+
+
+# 哈利波特 魔法覺醒,哈利波特魔法覺醒卡組,哈利波特 教學,索命咒,索命咒卡組,佛地魔卡組,啊哇呾喀呾啦,哈利波特 秒殺流,    Action-adventure game,Action game,Role-playing video game,Strategy video game,Video game culture
